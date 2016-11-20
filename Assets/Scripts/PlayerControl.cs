ae255bbe23330b8737a0830027bb1181efd921d5
Jason Turner <jturner@cs.dal.ca>
2016-11-19 20:23:51 -0400

    added animation transitions for walking and idle animations for player and simple script to control each

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public GameObject player;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			animator.SetBool ("walking", true);
+		} else {
+			animator.SetBool ("walking", false);
+		}
+	}
+}
