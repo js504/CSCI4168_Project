565aabb62681c8f1f1050e34ab6531ebe57aebb3
hassanar <hassanraza638@gmail.com>
2016-12-04 19:35:38 -0800

    Simple fire that tree catches for 15 sec and object that touches tree with fire, also catches. Will add more fire effects. And added music

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+	public Transform playerFire;
+
+	//public Transform fire;
+	// Use this for initialization
+	void Start () {
+		playerFire.GetComponent<ParticleSystem> ().Stop();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//playerFire.GetComponent<ParticleSystem> ().enableEmission = false;
+	}
+	void OnParticleCollision(GameObject other){
+		if (other.gameObject.name.Equals ("Particle System") || other.gameObject.name.Equals ("Particle System(Clone)")) {
+			Debug.Log ("hittttttttttt");
+			playerFire.GetComponent<ParticleSystem> ().Play();
+			StartCoroutine (stopFire ());
+		}
+	}
+	IEnumerator stopFire(){
+		yield return new WaitForSeconds (15);
+		playerFire.GetComponent<ParticleSystem> ().Stop();
+	}
+
+
+	public Transform fire; // drag your explosion prefab here
+	//public GameObject hold;
+	public void OnCollisionEnter(Collision other){
+		if(playerFire.GetComponent<ParticleSystem> ().isPlaying == true){
+		Instantiate(fire, other.transform.position, Quaternion.identity);
+		}
+		//Destroy(gameObject); // destroy the grenade
+		//Destroy(hold, 3); // delete the explosion after 3 seconds
+	}
+}
+
