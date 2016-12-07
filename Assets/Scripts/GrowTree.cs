6d4504eec5a7edbad70ca57fbdbd1412976d88b3
hassanar <hassanraza638@gmail.com>
2016-12-06 17:56:19 -0800

    There are cubes in level 2 that grow trees when players passes by them

---
+++ Assets/Scripts/GrowTree.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowTree : MonoBehaviour {
+
+	public GameObject tree;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name.Equals ("Player")) {
+			Instantiate (tree, other.transform.position, transform.rotation);
+		}
+	}
+}
